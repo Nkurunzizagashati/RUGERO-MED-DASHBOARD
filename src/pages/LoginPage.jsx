6286dee4ae64ebbebce22ee44279e84fefaee7fc
Fabrice <f.nkurunziz@alustudent.com>
2025-09-04 15:54:54 +0200

    change the login page background color

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -29,8 +29,8 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className="min-h-screen flex items-center justify-center bg-gray-100">
-			<div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+		<div className="min-h-screen flex items-center justify-center bg-rugero-secondary">
+			<div className="bg-rugero-lightGray shadow-lg rounded-lg p-8 w-full max-w-md">
 				<h2 className="text-2xl font-bold text-center mb-6">
 					Admin Login
 				</h2>
